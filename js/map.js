c25c279d41a63011af804e3ea1c1274fa9b2700e
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-12-12 19:47:27 +0100

    Include imagemapster

--- js/map.js
+++ js/map.js
@@ -1,3 +1,18 @@
-jQuery(function(){
-  jQuery('.drumkitmap').maphilight();
+
+
+$(document).ready(function(){
+  $('#dkit').mapster({
+    staticState: false,
+    scaleMap: true,
+    areas: [
+      {
+        key: "wood"// causes the mask to be included when this area is highlighted
+      },
+      {
+        key: "piezomask",
+        isMask: true                     // causes the inner circle to be treated as a mask, but only                                 // in the context of the "outer-circle-mask" group
+      }
+  // no special options needed for "inner-circle" - we want it to be treated normally on mouseover
+  ]
+  });
 });
