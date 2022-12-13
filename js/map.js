b90cbc18ffc40e5e12f3216fb83a68fa0a51d0c1
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-12-13 12:51:19 +0100

    update unassembled pad imagemap

--- js/map.js
+++ js/map.js
@@ -2,17 +2,14 @@
 
 $(document).ready(function(){
   $('#dkit').mapster({
-    staticState: false,
+    isSelectable: false,
     scaleMap: true,
+    fillColor: "990000",
+    mapKey: 'alt',
     areas: [
-      {
-        key: "wood"// causes the mask to be included when this area is highlighted
-      },
-      {
-        key: "piezomask",
-        isMask: true                     // causes the inner circle to be treated as a mask, but only                                 // in the context of the "outer-circle-mask" group
-      }
-  // no special options needed for "inner-circle" - we want it to be treated normally on mouseover
-  ]
+        {
+            mapKey: 'wood',
+        }
+    ]
   });
 });
