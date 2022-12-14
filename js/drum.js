13e97a1948646417951752e8eeeaf1cb5e8058d4
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-12-14 23:41:30 +0100

    Update interactive imagemap
    I apologise for drum.js
    Goodbye Noel :'(

---
+++ js/drum.js
@@ -0,0 +1,178 @@
+//please forgive me for what I am about to do
+
+function togglefoam() {
+  var x = document.getElementById("foaminfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("corkinfo").style.display === "block"){
+    document.getElementById("corkinfo").style.display = "none";
+  }
+
+  if (document.getElementById("woodinfo").style.display === "block"){
+    document.getElementById("woodinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pvcinfo").style.display === "block"){
+    document.getElementById("pvcinfo").style.display = "none";
+  }
+
+  if (document.getElementById("elecinfo").style.display === "block"){
+    document.getElementById("elecinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pinfo").style.display === "block"){
+    document.getElementById("pinfo").style.display = "none";
+  }
+}
+
+function togglecork() {
+  var x = document.getElementById("corkinfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("foaminfo").style.display === "block"){
+    document.getElementById("foaminfo").style.display = "none";
+  }
+
+  if (document.getElementById("woodinfo").style.display === "block"){
+    document.getElementById("woodinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pvcinfo").style.display === "block"){
+    document.getElementById("pvcinfo").style.display = "none";
+  }
+
+  if (document.getElementById("elecinfo").style.display === "block"){
+    document.getElementById("elecinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pinfo").style.display === "block"){
+    document.getElementById("pinfo").style.display = "none";
+  }
+}
+
+
+function togglepvc() {
+  var x = document.getElementById("pvcinfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("foaminfo").style.display === "block"){
+    document.getElementById("foaminfo").style.display = "none";
+  }
+
+  if (document.getElementById("woodinfo").style.display === "block"){
+    document.getElementById("woodinfo").style.display = "none";
+  }
+
+  if (document.getElementById("corkinfo").style.display === "block"){
+    document.getElementById("corkinfo").style.display = "none";
+  }
+
+  if (document.getElementById("elecinfo").style.display === "block"){
+    document.getElementById("elecinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pinfo").style.display === "block"){
+    document.getElementById("pinfo").style.display = "none";
+  }
+}
+
+
+function togglewood() {
+  var x = document.getElementById("woodinfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("foaminfo").style.display === "block"){
+    document.getElementById("foaminfo").style.display = "none";
+  }
+
+  if (document.getElementById("corkinfo").style.display === "block"){
+    document.getElementById("corkinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pvcinfo").style.display === "block"){
+    document.getElementById("pvcinfo").style.display = "none";
+  }
+
+  if (document.getElementById("elecinfo").style.display === "block"){
+    document.getElementById("elecinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pinfo").style.display === "block"){
+    document.getElementById("pinfo").style.display = "none";
+  }
+}
+
+
+function togglep() {
+  var x = document.getElementById("pinfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("foaminfo").style.display === "block"){
+    document.getElementById("foaminfo").style.display = "none";
+  }
+
+  if (document.getElementById("corkinfo").style.display === "block"){
+    document.getElementById("corkinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pvcinfo").style.display === "block"){
+    document.getElementById("pvcinfo").style.display = "none";
+  }
+
+  if (document.getElementById("elecinfo").style.display === "block"){
+    document.getElementById("elecinfo").style.display = "none";
+  }
+
+  if (document.getElementById("woodinfo").style.display === "block"){
+    document.getElementById("woodinfo").style.display = "none";
+  }
+}
+
+function togglemodule() {
+  var x = document.getElementById("elecinfo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+  if (document.getElementById("foaminfo").style.display === "block"){
+    document.getElementById("foaminfo").style.display = "none";
+  }
+
+  if (document.getElementById("corkinfo").style.display === "block"){
+    document.getElementById("corkinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pvcinfo").style.display === "block"){
+    document.getElementById("pvcinfo").style.display = "none";
+  }
+
+  if (document.getElementById("woodinfo").style.display === "block"){
+    document.getElementById("woodinfo").style.display = "none";
+  }
+
+  if (document.getElementById("pinfo").style.display === "block"){
+    document.getElementById("pinfo").style.display = "none";
+  }
+}
